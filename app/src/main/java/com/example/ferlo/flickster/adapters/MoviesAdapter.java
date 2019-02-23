14399dba4e60b157a51195bbea506b59f6f80eb7
Olu Owolabi <oluwaferanmiowolabi@gmail.com>
2019-02-23 09:41:38 -0600

    initial commit

--- app/src/main/java/com/example/ferlo/flickster/adapters/MoviesAdapter.java
+++ app/src/main/java/com/example/ferlo/flickster/adapters/MoviesAdapter.java
@@ -1,7 +1,7 @@
 package com.example.ferlo.flickster.adapters;
 
 import android.content.Context;
-import android.content.res.Configuration;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -9,11 +9,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.ferlo.flickster.DetailActivity;
 import com.example.ferlo.flickster.R;
 import com.example.ferlo.flickster.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -52,21 +56,34 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
+        RelativeLayout container;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             String imageUrl = movie.getPosterPath();
-            if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
-                imageUrl = movie.getBackdropPath();
-            }
+           // if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+               // imageUrl = movie.getBackdropPath();
+           // }
             Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent i = new Intent(context,DetailActivity.class);
+
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
